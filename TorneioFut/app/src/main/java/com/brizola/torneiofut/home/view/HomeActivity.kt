e5d58f89351261d84a3052f36e519c0db51d3e36
Lucas BJ <lucasbrjacob@outlook.com>
2023-05-10 13:47:19 -0300

    first configuration + new match layout + new team layout

---
+++ TorneioFut/app/src/main/java/com/brizola/torneiofut/home/view/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.brizola.torneiofut.home.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.brizola.torneiofut.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ TorneioFut/app/src/main/java/com/brizola/torneiofut/team/view/NewTeamActivity.kt
@@ -0,0 +1,14 @@
+package com.brizola.torneiofut.team.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.brizola.torneiofut.databinding.ActivityNewTeamBinding
+
+class NewTeamActivity : AppCompatActivity() {
+
+}
